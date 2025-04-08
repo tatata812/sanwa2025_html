864728a144d4575276ee9dc212c16a1993435530
tatata812 <75353660+tatata812@users.noreply.github.com>
2025-04-08 18:13:41 +0900

    修正

--- assets/js/main.js
+++ assets/js/main.js
@@ -388,9 +388,46 @@ $(function () {
       }, 500); // スライドの transition 時間
     }
   }, 3000);
+
+
+  // メインビジュアル　スキップボタン
+  $('.gsap-main__skip-btn').on('click', function(e) {
+    e.preventDefault(); // デフォルトのアンカーリンクの動作をキャンセル
+
+    const target = $('#main-visual');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 800, 'swing'); // スムーズスクロールのアニメーション（durationとeasingは調整可能）
+    }
+  });
 })
 
 
-// GSAP
+// GSAPメインビジュアル
+const wrapper = document.querySelector('#wrapper');
+if(wrapper) {
+    // gsap.registerPlugin(ScrollTrigger); // npm/yarnの際に必要
+    const panels = gsap.utils.toArray('.panel');
+    const wrapperWidth = wrapper.offsetWidth;
+    /**
+    * 横スクロール開始
+    */
+    gsap.to( panels, {
+        xPercent: -100 * (panels.length - 1), // transformX
+        ease: "none", // easingの設定
+        scrollTrigger: { // scrollTrigger
+            trigger: wrapper, // アニメーションの対象となる要素
+            pin: true, // 要素を固定する
+            scrub: 1, // スクロールとアニメーションを同期させる。数値で秒数の設定に
+            snap: { // スナップスクロールにする
+                snapTo: 1 / ( panels.length - 1 ), // スナップで移動させる位置
+                duration: {min: .4, max: .6}, // スナップで移動する際の遅延時間
+                ease: "none" // easing
+            },
+            end: () => "+=" + wrapperWidth // アニメーションの終了タイミング
+        }
+    })
+}
 
 
